2dff5b64b9d7c362fa66730a27380811b6f45cd2
najaad <46876019+najaad@users.noreply.github.com>
2025-03-15 11:42:20 -0400

    Update Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+ <template>
+  <div class="hello">
+    <h1>Hello Shannon!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
+
+<style>
+.hello {
+  font-family: 'Inter', sans-serif;
+  text-align: center;
+  margin-top: 20vh;
+}
+</style>
