005c7a36c354946fd1c3a72ae59eab81f1ff5954
najaad <46876019+najaad@users.noreply.github.com>
2025-03-15 12:08:33 -0400

    more fixes for multi word for consistency

--- src/router.js
+++ src/router.js
@@ -2,13 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "./views/HomeView.vue";
 import Bookshelf from "./views/BookshelfView.vue";
 import Progress from "./views/ProgressView.vue";
-import AtoZ from "./views/AtoZ.vue";
+import AtoZ from "./views/AtoZView.vue";
 
 const routes = [
-  { path: "/", component: Home },
-  { path: "/bookshelf", component: Bookshelf },
-  { path: "/progress", component: Progress },
-  { path: "/atoz", component: AtoZ }
+  { path: "/", component: HomeView },
+  { path: "/bookshelf", component: BookshelfView },
+  { path: "/progress", component: ProgressView },
+  { path: "/atoz", component: AtoZView }
 ];
 
 const router = createRouter({
