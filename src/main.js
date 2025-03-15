24639df3ae9ed940cbb91f9b21ab1bfaa8ee4256
najaad <46876019+najaad@users.noreply.github.com>
2025-03-15 11:53:26 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 import './assets/main.css';
+import router from "./router"; // Import router
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router); // Register router
+app.mount("#app");
